1cc100e492d7517dc5a0203e6a0e46d52af7258d
Your Name <you@example.com>
2015-11-21 00:03:17 +0200

    fix footer position[3];add inline inputes;join doesnt work

--- init/forms.py
+++ init/forms.py
@@ -1,33 +1,47 @@
 from django import forms
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit, Layout, Field
+from crispy_forms.layout import Submit, Layout, Field, Div
+from crispy_forms.bootstrap import InlineField
 
 
 class MyWidget(forms.widgets.MultiWidget):
     def __init__(self, attrs=None):
-
         _widgets = (
-            forms.widgets.TextInput(attrs={}),
-            forms.widgets.TextInput(),
-            forms.widgets.TextInput(),
+            forms.widgets.TextInput(attrs={'size': '4', 'placeholder': '###', 'maxlength': '3'}),
+            forms.widgets.TextInput(attrs={'size': '4', 'placeholder': '###', 'maxlength': '3'}),
+            forms.widgets.TextInput(attrs={'size': '6', 'placeholder': '####', 'maxlength': '4'}),
         )
         super(MyWidget, self).__init__(_widgets, attrs)
 
     def decompress(self, value):
         if value:
-            return [value.day, value.month, value.year]
+            print(value)
+            #return [value.day, value.month, value.year]
+            return ''.join(value)
         return [None, None, None]
 
     def format_output(self, rendered_widgets):
-        return ''.join(rendered_widgets)
+        return (
+                '<div class="row phone-inputs">'
+                '<div class="col-lg-12">'
+                '<div class="form-inline">'
+                '<div class="col-md-3 col-xs-3 col-sm-10">%s</div>'
+                '<div class="col-md-1 col-xs-3 col-sm-10 text-center">-</div>'
+                '<div class="col-md-3 col-xs-3 col-sm-2">%s</div>'
+                '<div class="col-md-1 col-xs-3 col-sm-2 text-center">-</div>'
+                '<div class="col-md-3 col-xs-4 col-sm-2">%s</div>'
+                '</div>'
+                '</div>'
+                '</div>'
+               ) % tuple(rendered_widgets)
+        # return ''.join(rendered_widgets)
 
 
 class DemoForm(forms.Form):
-
     first_name = forms.CharField()
     last_name = forms.CharField()
     email = forms.EmailField()
-    phone = forms.CharField(required=False, widget=MyWidget)
+    phone = forms.CharField(required=False, widget=MyWidget, label='')
     url = forms.URLField(required=False)
 
     def __init__(self, *args, **kwargs):
@@ -37,29 +51,31 @@ class DemoForm(forms.Form):
         self.helper.form_id = 'demo_form'
         self.helper.form_action = '#'
         self.helper.form_class = 'form-horizontal'
-        self.helper.label_class = 'col-md-2'
         self.helper.field_class = 'col-md-7'
 
         self.helper.add_input(Submit('submit', 'Request demo',
                                      css_class='btn btn-default btn-md'))
 
         self.helper.layout = Layout(
-            Field(
-                'first_name', placeholder='First name', css_class='col-lg-1'
+
+            InlineField(
+                'first_name'
             ),
-            Field(
-                'last_name', placeholder='Last name'
+            InlineField(
+                'last_name'
             )
             ,
-            Field(
-                'email', placeholder='Email'
+            InlineField(
+                'email'
             )
             ,
-            Field(
-                'phone', placeholder='xxx'
+            Div(
+                InlineField(
+                    'phone'
+                ), css_class='form-inline'
             )
             ,
-            Field(
+            InlineField(
                 'url', placeholder='Website/URL (optional)'
             )
         )
--- init/views.py
+++ init/views.py
@@ -27,7 +27,6 @@ class HomeView(View):
             'title': self.title,
         }
         if form.is_valid():
-            print('form valid')
             return redirect(self.get_success_url())
         return TemplateResponse(request, self.template_name, context)
 
