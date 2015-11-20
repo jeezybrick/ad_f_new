1d3eaaab00dc958e5a899d98eac5c9b5aeae5ce0
Your Name <you@example.com>
2015-11-20 22:40:06 +0200

    fix footer position[2];add my widget

--- init/forms.py
+++ init/forms.py
@@ -3,18 +3,38 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit, Layout, Field
 
 
+class MyWidget(forms.widgets.MultiWidget):
+    def __init__(self, attrs=None):
+
+        _widgets = (
+            forms.widgets.TextInput(attrs={}),
+            forms.widgets.TextInput(),
+            forms.widgets.TextInput(),
+        )
+        super(MyWidget, self).__init__(_widgets, attrs)
+
+    def decompress(self, value):
+        if value:
+            return [value.day, value.month, value.year]
+        return [None, None, None]
+
+    def format_output(self, rendered_widgets):
+        return ''.join(rendered_widgets)
+
+
 class DemoForm(forms.Form):
 
     first_name = forms.CharField()
     last_name = forms.CharField()
     email = forms.EmailField()
-    phone = forms.CharField(required=False)
+    phone = forms.CharField(required=False, widget=MyWidget)
     url = forms.URLField(required=False)
 
     def __init__(self, *args, **kwargs):
         super(DemoForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_method = 'post'
+        self.helper.form_id = 'demo_form'
         self.helper.form_action = '#'
         self.helper.form_class = 'form-horizontal'
         self.helper.label_class = 'col-md-2'
@@ -25,7 +45,7 @@ class DemoForm(forms.Form):
 
         self.helper.layout = Layout(
             Field(
-                'first_name', placeholder='First name'
+                'first_name', placeholder='First name', css_class='col-lg-1'
             ),
             Field(
                 'last_name', placeholder='Last name'
@@ -33,6 +53,10 @@ class DemoForm(forms.Form):
             ,
             Field(
                 'email', placeholder='Email'
+            )
+            ,
+            Field(
+                'phone', placeholder='xxx'
             )
             ,
             Field(
