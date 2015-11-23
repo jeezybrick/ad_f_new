0690cba99e72086cd3220f941e594f00f1306486
jeezybrick <smooker14@gmail.com>
2015-11-23 15:28:25 +0200

    remove email pass

--- Fits/settings.py
+++ Fits/settings.py
@@ -113,6 +113,6 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'smooker14@gmail.com'
-EMAIL_HOST_PASSWORD = '16931693'
+EMAIL_HOST_PASSWORD = 'password'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
