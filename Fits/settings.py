237166402fe657500575401f4f5ea9566516ffe4
Andrey Statsenko <smooker14@gmail.com>
2015-11-24 10:03:46 +0200

    Update settings.py

--- Fits/settings.py
+++ Fits/settings.py
@@ -105,6 +105,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
 # STATICFILES_DIRS = ('static', )
 
+# For media
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = '/media/'
+
 # bootstrap3 for crispy forms
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
